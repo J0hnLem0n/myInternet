bda0dc85ec73204d1b458964fce19cd4f6f37f8b
JohnLemon <nikolaevee@gmail.com>
2018-03-10 22:49:23 +0600

    pre scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './modules/material/material.module';
 
 import { AppComponent } from './app.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
+import { LeftMenuComponent } from './workspace/left-menu/left-menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkspaceComponent,
+    LeftMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatSidenavModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule
+  ]
+})
+export class MaterialModule {}
