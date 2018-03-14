abcc7789cadc81b964c603d3285f04105395245b
JohnLemon <nikolaevee@gmail.com>
2018-03-15 01:44:20 +0600

    refactoring

--- src/app/devices/move-objects/move-object.service.ts
+++ src/app/devices/move-objects/move-object.service.ts
@@ -3,24 +3,36 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class MoveObjectService {
-    private object;
+  private object: HTMLObjectElement;
 
-    public createObject() {
-        let svgDocument, svg;
+  public createObject(event) {
+    let svgDocument, svg;
+    const self = this;
 
-        this.object = document.createElement('object');
-        this.object.style.position = 'absolute';
-        this.object.style.zIndex = 1000;
-        this.object.addEventListener('load', function () {
-            svgDocument = this.contentDocument;
-            svg = svgDocument.querySelector('svg');
-        });
-        this.object.data = 'assets/img/material/ic_add_a_photo_24px.svg';
-        document.body.appendChild(this.object);
+    this.object = document.createElement('object');
+    this.object.style.position = 'absolute';
+    this.object.style.zIndex = '1000';
+    this.object.data = 'assets/img/material/ic_add_a_photo_24px.svg';
+    document.body.appendChild(this.object);
 
-        return this.object;
-    }
-    public removeObject() {
-        this.object.remove();
-    }
+    document.onmousemove = function(e) {
+      self.moveAt(e);
+    };
+
+    this.object.addEventListener('load', function () {
+      svgDocument = this.contentDocument;
+      svg = svgDocument.querySelector('svg');
+    });
+
+    this.moveAt(event);
+  }
+
+  public removeObject() {
+    this.object.remove();
+  }
+
+  private moveAt(event) {
+    this.object.style.left = event.pageX - this.object.offsetWidth / 2 + 'px';
+    this.object.style.top = event.pageY - this.object.offsetHeight / 2 + 'px';
+  }
 }
--- src/app/devices/shared/directives/device-drag.directive.ts
+++ src/app/devices/shared/directives/device-drag.directive.ts
@@ -9,9 +9,12 @@ import { MoveObjectService } from '../../move-objects/move-object.service';
 export class DeviceDragDirective {
   constructor(private moveObjectService: MoveObjectService) {}
 
-  @HostListener('mousedown') onMouseDown() {
-    this.moveObjectService.createObject();
+  @HostListener('mousedown', ['$event.target']) onMouseDown() {
+    this.moveObjectService.createObject(event);
   }
+  // @HostListener('mousemove', ['$event.target']) onMouseDown() {
+  //   this.moveObjectService.createObject(event);
+  // }
   @HostListener('mouseup') onMouseUp() {
     this.moveObjectService.removeObject();
     console.log(this.moveObjectService);
