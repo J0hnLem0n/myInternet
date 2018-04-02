45c77dcbad0014619bb4748871a929f02f2948fd
ENikolaev <ENikolaev@luxoft.com>
2018-04-02 16:27:21 +0600

    refactor

--- src/app/workspace/shared/d3-workspace.service.ts
+++ src/app/workspace/shared/d3-workspace.service.ts
@@ -6,7 +6,7 @@ export class D3WorkspaceService {
   private svg;
 
   public initWorkspace(): D3WorkspaceService {
-    /**TODO: отвязаться от ид #workspace */
+    /**TODO: отвязаться от ид #workspace (создать директиву)*/
     this.svg = d3.select('#workspace').append('svg');
     const width = '100%';
     const height = '100%';
@@ -21,11 +21,23 @@ export class D3WorkspaceService {
     /**TODO: Проработать точность координат*/
     const screenX = parseInt(obj.style.left, 10) - (imgSize + (24 * 2));
     const screenY = parseInt(obj.style.top, 10) - (imgSize + 24);
+    /**TODO: Переместить в метод + посмотреть возможность отказаться от замыканий*/
+    const dragMove = function() {
+      const x = d3.event.x;
+      const y = d3.event.y;
+      d3.select(this)
+        .attr('x', x)
+        .attr('y', y);
+    };
+    const drag = d3.drag()
+      .on('drag', dragMove);
+
     this.svg.append('svg:image')
       .attr('xlink:href', imgSrc)
       .attr('width', imgSize)
       .attr('height', imgSize)
       .attr('x', screenX)
+      .call(drag)
       .attr('y', screenY);
     return this;
   }
